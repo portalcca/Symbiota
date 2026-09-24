3d56c6c79733fb547886149d491f2b31b63139d3
ksorellana <kasaore@gmail.com>
2026-09-24 12:28:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -27,8 +27,8 @@ header('Content-Type: text/html; charset=' . $CHARSET);
 			?>
 			<div>
 				<h1 class="headline">Bienvenidos al Portal de Colecciones de Centroamérica</h1>
-				<p>Esta plataforma está diseñada como un recurso colaborativo para la digitalización y movilización de colecciones de biodiversidad de la región Centroamericana. Además, el portal facilita el uso de herramientas interactivas para la generación de mapas, listados de especies y claves de identificación. La información también puede ser movilizada a la Instalación Global de Información de Biodiversidad -GBIF-, desde donde puede alimentar a otros agregadores de información locales e internacionales. Los datos añadidos al portal están disponibles para ser utilizados por investigadores, estudiantes y público en general, por lo que se insta a citar adecuadamente su origen. 			
-Para más información o integrar sus colecciones, pueden escribir a portalcentroamerica@gmail.com.</p>
+				<p>Esta plataforma está diseñada como un recurso colaborativo para la digitalización y movilización de colecciones de biodiversidad de la región Centroamericana. Además, el portal facilita el uso de herramientas interactivas para la generación de mapas, listados de especies y claves de identificación. La información también puede ser movilizada a la Instalación Global de Información de Biodiversidad -GBIF-, desde donde puede alimentar a otros agregadores de información locales e internacionales. Los datos añadidos al portal están disponibles para ser utilizados por investigadores, estudiantes y público en general, por lo que se insta a citar adecuadamente su origen.</p> 			
+<p> Para más información o integrar sus colecciones, pueden escribir a portalcentroamerica@gmail.com.</p>
 			</div>
 			<?php
 		}
@@ -44,11 +44,11 @@ Para más información o integrar sus colecciones, pueden escribir a portalcentr
 			//Default Language
 			?>
 			<div>
-				<h1>Welcome</h1>
+				<h1>Welcome to the Central American Collections Portal</h1>
 				<p>
-					This data portal has been established to promote collaborative... Replace
-					with introductory text in English. If the portal is not meant to be
-					multilingual, remove the unneeded language sections
+					This platform is designed as a collaborative resource for the digitization and mobilization of biodiversity collections in the Central American region. Furthermore, the portal facilitates the use of interactive tools for generating maps, species lists, and identification keys. The information can also be published to the Global Biodiversity Information Facility (GBIF), from where it can be harvested by other local and international data aggregators. Data added to the portal is available for use by researchers, students, and the general public. Users are encouraged to properly cite the source. </p>
+<p> For more information or to add a collection, please contact the portal administrators at portalcentroamerica@gmail.com.
+
 				</p>
 			</div>
 			<?php
